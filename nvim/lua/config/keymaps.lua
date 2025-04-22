f35dc80b5cc304a9ef8961966f3ae8a6437ca2d5
Yar Kravtsov <yarlson@gmail.com>
2025-04-22 20:39:35 +0300

    feat(ui): integrate Bufferline for enhanced buffer management

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,12 +1,32 @@
 local keymap = vim.keymap.set
 
 -- Buffer navigation
-keymap('n', '<C-l>', function() vim.cmd.bnext() end, { desc = 'Next Buffer' })
-keymap('n', '<C-h>', function() vim.cmd.bprevious() end, { desc = 'Previous Buffer' })
+-- Standard Neovim buffer nav (still useful)
+keymap('n', '<C-l>', function()
+  vim.cmd.bnext()
+end, { desc = 'Next Buffer (Standard)' })
+keymap('n', '<C-h>', function()
+  vim.cmd.bprevious()
+end, { desc = 'Previous Buffer (Standard)' })
 
--- Buffer deletion
-keymap('n', '<leader>bd', function() vim.cmd.Bdelete() end, {
-  desc = 'Delete Current Buffer',
-})
+-- Bufferline specific navigation
+keymap('n', '[b', '<Cmd>BufferLineCyclePrev<CR>', { desc = 'Previous Buffer (Bufferline)' })
+keymap('n', ']b', '<Cmd>BufferLineCycleNext<CR>', { desc = 'Next Buffer (Bufferline)' })
+
+-- Buffer closing
+keymap('n', '<leader>bd', function()
+  vim.cmd.Bdelete()
+end, { desc = 'Delete Current Buffer (Bdelete)' }) -- Keep original Bdelete too
+
+-- Jump to buffer by number (Bufferline)
+for i = 1, 9 do
+  keymap('n', '<leader>' .. i, function()
+    vim.cmd('BufferLineGoToBuffer ' .. i)
+  end, { desc = 'Go to Buffer ' .. i })
+end
+
+-- Project navigation (using Telescope which integrates with project.nvim)
+-- Keymap <leader>fp is already defined in lua/plugins/telescope.lua for finding projects
 
 -- Add other keymaps from your original init.lua here
+
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -19,4 +19,38 @@ return {
       })
     end,
   },
+  {
+    'akinsho/bufferline.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    version = "*",
+    opts = {
+      options = {
+        mode = "buffers",
+        numbers = "ordinal",
+        close_command = "Bdelete! %d",
+        right_mouse_command = "Bdelete! %d",
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " " or (e == "warning" and " " or " ")
+            s = s .. n .. sym
+          end
+          return s
+        end,
+        offsets = {
+          {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+          },
+        },
+        separator_style = "thin",
+        always_show_bufferline = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+      },
+    },
+  },
 }
