c1ce22f203cbe7365ea1674ad93d36d8dd8e1f4d
Yar Kravtsov <yarlson@gmail.com>
2026-05-26 11:06:07 +0300

    lnk: sync configuration files

--- .config/tmux-powerline/themes/tabs-only.sh
+++ .config/tmux-powerline/themes/tabs-only.sh
@@ -4,6 +4,7 @@ flexoki_black="#100f0f"
 flexoki_base_950="#1c1b1a"
 flexoki_base_500="#878580"
 flexoki_base_200="#cecdc3"
+flexoki_status_fg="$flexoki_base_200"
 flexoki_blue_2="#4385be"
 
 TMUX_POWERLINE_SEPARATOR_THIN="|"
@@ -12,7 +13,10 @@ TMUX_POWERLINE_DEFAULT_BACKGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_BACKGROUND_COLO
 TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR=${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR:-$flexoki_base_200}
 
 TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=()
-TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=()
+TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
+	"date $flexoki_base_950 $flexoki_status_fg default_separator no_sep_bg_color no_sep_fg_color left_disable separator_disable"
+	"time $flexoki_base_950 $flexoki_status_fg default_separator no_sep_bg_color no_sep_fg_color both_disable separator_disable"
+)
 
 TMUX_POWERLINE_WINDOW_STATUS_CURRENT=(
 	"#[fg=$flexoki_black,bg=$flexoki_blue_2,bold]"
